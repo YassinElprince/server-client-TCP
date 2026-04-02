a3930eb1533827a3af70cc74c916fb58a4fa7266
YassinElprince <yassin.elprince2005@gmail.com>
2026-04-02 20:10:42 +0200

    Initialize client.c with includes and constants
    Add necessary includes and define constants for networking and encryption.

--- client.c
+++ client.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <openssl/evp.h>
+#define PORT 8080
+#define BUFFER_SIZE 1024
+unsigned char KEY[16] = "0123456789abcdef";
+unsigned char IV[16]  = "abcdef9876543210";
