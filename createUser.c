176499c2da5cc860e92db769a8f215e82373cccb
YassinElprince <yassin.elprince2005@gmail.com>
2026-04-02 17:53:42 +0200

    Implement user creation and save to file
    Added functionality to save username and hashed password to users.txt.

--- createUser.c
+++ createUser.c
@@ -12,3 +12,11 @@ int main() {
     scanf("%255s", password);
     password[strcspn(password, "\r\n")] = 0;
     SHA256((unsigned char *)password, strlen(password), hash);
+    FILE *fp = fopen("users.txt", "a");
+    fprintf(fp, "%s ", username);
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        fprintf(fp, "%02x", hash[i]);}
+    fprintf(fp, "\n");
+    fclose(fp);
+    printf("User added successfully.\n");
+    return 0;}
