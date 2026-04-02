8353f99c65d581b4de1956bfcf58983e75234511
YassinElprince <yassin.elprince2005@gmail.com>
2026-04-02 21:10:04 +0200

    Implement AES-128-CBC encryption and decryption
    Added encryption and decryption functions using AES-128-CBC.

--- client.c
+++ client.c
@@ -8,3 +8,25 @@
 #define BUFFER_SIZE 1024
 unsigned char KEY[16] = "0123456789abcdef";
 unsigned char IV[16]  = "abcdef9876543210";
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *ciphertext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, ciphertext_len;
+    EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, KEY, IV);
+    EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len);
+    ciphertext_len = len;
+    EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+    ciphertext_len += len;
+    EVP_CIPHER_CTX_free(ctx);
+    return ciphertext_len;}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext) {
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    int len, plaintext_len;
+    EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, KEY, IV);
+    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len);
+    plaintext_len = len;
+    EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+    plaintext_len += len;
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext_len;}
